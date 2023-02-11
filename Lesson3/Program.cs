b623930b565ffb5a7a09939d8f579956a513aada
patriarhiya <patriarhiya@mail.ru>
2023-02-11 16:32:18 +0300

    add Lesson3

---
+++ Lesson3/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Clear();
+Console.Write("Введите число, чтобы вывести от -число до число: ");
+int N = int.Parse(Console.ReadLine());
+for (int i = -N; i <= N; i++)
+{
+    Console.Write(i + " ");
+}
