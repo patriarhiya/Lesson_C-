5a33f3f3c93cccbfee68c86bb61cc54230f6c020
patriarhiya <patriarhiya@mail.ru>
2023-02-11 15:15:28 +0300

    add Lesson1

---
+++ Lesson1/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число 1: ");
+int number1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число 2: ");
+int number2 = int.Parse(Console.ReadLine());
+int sqr = number1 * number1;
+Console.WriteLine($"{sqr == number2}");
+if(sqr == number2){Console.WriteLine($"Квадрат числа 1 ({number1}) равно числу номер 2({number2})");}
+else{Console.WriteLine($"Квадрат числа 1 ({number1}) не равно числу номер 2 ({number2})");}
+
+int sqr1 = Convert.ToInt32(Math.Pow(number1, 2));
+Console.WriteLine($"Квадрат числа {number1} -> {sqr1}");
