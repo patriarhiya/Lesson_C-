fe68d8a739147143a6c02f526deb8a8c6fa5dda1
patriarhiya <patriarhiya@mail.ru>
2023-02-11 16:34:39 +0300

    add Lesson3-1

--- Lesson3/Program.cs
+++ Lesson3/Program.cs
@@ -5,3 +5,12 @@ for (int i = -N; i <= N; i++)
 {
     Console.Write(i + " ");
 }
+
+Console.Clear();
+Console.Write("Введите число, чтобы вывести от -число до число: ");
+int N = int.Parse(Console.ReadLine());
+int B = -N;
+while(B <= N)
+{
+    Console.Write($"{B} ");
+}
