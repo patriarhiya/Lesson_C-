3b3acde361706538d81931f8aa5bb8fda73ea638
patriarhiya <patriarhiya@mail.ru>
2023-02-12 14:10:47 +0300

    add Example 008-011

---
+++ Example010_FindArray/Program.cs
@@ -0,0 +1,13 @@
+﻿int[] array = {1,2,34,4,4,5,69,7,8,9};
+int n = array.Length;
+
+int find = 4;
+int i = 0;
+while(i < n){
+
+    if(array[i]==find){
+        Console.WriteLine(i+1);
+        break;
+    }
+    i++;
+}
---
+++ Example011_FindArrayRandom/Program.cs
@@ -0,0 +1,24 @@
+﻿void FillArray(int[] collection)
+{
+     int lenght = collection.Length;
+     int i = 0;
+     while(i < lenght){
+        collection[i] = new Random().Next(1, 100);
+        i++;
+     }
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while(position<count ){
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+int[] array = new int[10];
+
+FillArray(array);
+PrintArray(array);
