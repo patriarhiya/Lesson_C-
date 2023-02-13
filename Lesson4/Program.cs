efdd8364d0fd66b77e58b0b880c7cbd8d995a445
patriarhiya <patriarhiya@mail.ru>
2023-02-13 20:03:19 +0300

    add seminar 2

---
+++ Lesson4/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Clear();
+int num = new Random().Next(10,100);
+int a1 = num / 10;
+int a2 = num % 10;
+int max = a1;
+if (max < a2) max = a2;
+else Console.Write(max);
---
+++ Lesson5/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Clear();
+int num = new Random().Next(100,1000);
+int a1 = num / 100;
+int a2 = num % 10;
+int result = a1*10 + a2;
+Console.WriteLine($"Для числа {num} ответ: {result}"); 
