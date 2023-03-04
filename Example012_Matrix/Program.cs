2973cb7cb8aca9f271908b6241cfa954b0d615f7
patriarhiya <patriarhiya@mail.ru>
2023-03-04 14:22:51 +0300

    Example012

---
+++ Example012_Matrix/Program.cs
@@ -0,0 +1,11 @@
+﻿string[,] table = new string[2, 5];
+table[1, 2] = "слово";
+for (int rows = 0; rows < 2; rows++)
+{
+    for (int columns = 0; columns < 5; columns++)
+    {
+        Console.WriteLine($"{rows+1} {columns+1}:{table[rows, columns]}");
+    }    
+}
+
+int[,] matrix = new int[3, 4];
